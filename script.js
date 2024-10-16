22365e1b56ab5451a1e0ea0dcf3069a96fc0786b
achdianaja <rizkiachdian@gmail.com>
2024-10-16 18:56:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// var btnSubmit = document.getElementById('btnSubmit');
+
+
+function btnSubmit(){
+    const x = document.getElementById('name').value;
+    const y = document.getElementById('password').value;
+    const z = document.getElementById('umur').value;
+
+    alert("name : " + x + " passord : " + y + "  umur : " + z)
+}
